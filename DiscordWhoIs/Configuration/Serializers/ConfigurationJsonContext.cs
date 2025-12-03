7f20e75c60e867e5aadcb5613050ae0a6b7f46d3
PorterSilver <31625469+JFletchersil@users.noreply.github.com>
2025-12-03 20:46:49 +0000

    Add Missing Serializers

--- DiscordWhoIs/Configuration/Serializers/ConfigurationJsonContext.cs
+++ DiscordWhoIs/Configuration/Serializers/ConfigurationJsonContext.cs
@@ -4,9 +4,11 @@ using System.Text.Json.Serialization;
 namespace DiscordWhoIs.Databases.Serializers
 {
     [JsonSerializable(typeof(AliasConfiguration))]
+    [JsonSerializable(typeof(Ao3Configuration))]
     [JsonSerializable(typeof(CacheConfiguration))]
     [JsonSerializable(typeof(DiscordConfiguration))]
     [JsonSerializable(typeof(FandomConfiguration))]
+    [JsonSerializable(typeof(ProxyConfiguration))]
     public partial class ConfigurationJsonContext : JsonSerializerContext { }
 
 }
